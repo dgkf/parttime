127f4680c046008b0e08dfd7d90ec7c6b4db0fcd
dgkf <18220321+dgkf@users.noreply.github.com>
2019-09-07 22:31:26 -0700

    resolved R CMD Check notes

--- R/includes.R
+++ R/includes.R
@@ -14,8 +14,8 @@ includes <- function(e1, e2) {
 #' 
 #' @inheritParams includes
 #' 
-#' @rdname includes
-#' @export 
+#' @method includes partial_time
+#' @export
 includes.partial_time <- function(e1, e2) {
   UseMethod("includes.partial_time", e2)
 }
@@ -26,13 +26,14 @@ includes.partial_time <- function(e1, e2) {
 #'
 #' @inheritParams includes
 #'
-#' @export
 #' @examples
 #' x <- as.parttime(c("2019", "2019-03-01", "2019-03",    "2018",    "",     "2018", ""))
 #' y <- as.parttime(c("2019", "2019-03",    "2019-03-01", "2016-05", "2018", "",     ""))
 #'
 #' includes(x, y)
 #'
+#' @method includes.partial_time partial_time
+#' @export
 includes.partial_time.partial_time <- function(e1, e2) {
   es <- vctrs::vec_recycle_common(e1, e2)
   e1f <- vctrs::field(es[[1]], "pttm_mat")
