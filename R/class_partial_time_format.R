d25f537bddfec372b38b8e76a704a6c2bb4210bf
dgkf <18220321+dgkf@users.noreply.github.com>
2022-04-21 14:52:26 -0700

    removing isFALSE uses

--- R/class_partial_time_format.R
+++ R/class_partial_time_format.R
@@ -3,7 +3,7 @@
 format.partial_time <- function(x, ..., quote = TRUE) {
   x_str <- vector("character", length(x))
   x_str[] <- pillar::style_na('NA')
-  
+
   xna <- is.na(x)
   x_str[!xna]<- sprintf(
     if (quote) '"%s"' else '%s',
@@ -15,37 +15,37 @@ format.partial_time <- function(x, ..., quote = TRUE) {
 
 #' @importFrom pillar style_subtle style_na
 #' @importFrom crayon col_substring
-format_field_matrix <- function(x, 
+format_field_matrix <- function(x,
     verbose = getOption("parttime.print_verbose", FALSE)) {
-  
+
   tz <- .i(x, 2, "tzhour", "tzmin")
   tzs <- all_tz(x)
-  
+
   x_omit <- FALSE
-  if (isFALSE(verbose)) {
-    x_omit <- matrix(logical(), 
-      nrow = nrow(x), 
-      ncol = ncol(x), 
+  if (!isTRUE(verbose)) {
+    x_omit <- matrix(logical(),
+      nrow = nrow(x),
+      ncol = ncol(x),
       dimnames = dimnames(x))
-    
+
     x_omit[,tz] <- all(is.na(x[,tz])) | all(x[,tz] == 0)
     x_omit[,-tz] <- col(x[,-tz,drop = FALSE]) > apply(x[,-tz,drop = FALSE], 1, Position, f = Negate(is.na), right = TRUE)
   }
-  
-  x_str <- matrix(character(), 
-    nrow = nrow(x), 
-    ncol = ncol(x), 
+
+  x_str <- matrix(character(),
+    nrow = nrow(x),
+    ncol = ncol(x),
     dimnames = dimnames(x))
-  
+
   # date
   x_str[,"year"] <- format_field(x[,"year"], 4)
   x_str[,"month"] <- paste0(
-    pillar::style_subtle('-'), 
+    pillar::style_subtle('-'),
     format_field(x[,"month"], 2, TRUE))
   x_str[,"day"] <- paste0(
-    pillar::style_subtle('-'), 
+    pillar::style_subtle('-'),
     format_field(x[,"day"], 2, TRUE))
-  
+
   # time
   x_str[,"hour"] <- paste0(' ', format_field(x[,"hour"], 2, TRUE))
   x_str[,"min"] <- paste0(pillar::style_subtle(':'), format_field(x[,"min"], 2))
@@ -53,7 +53,7 @@ format_field_matrix <- function(x,
   x_str[,"secfrac"] <- paste0(
     pillar::style_subtle('.'),
     crayon::col_substring(format_field(x[,"secfrac"], 3, fmt = "%.03f"), 3))
-  
+
   # optional timezone (timespan/duration have no tz elements)
   if (is.na(tzs)) {
     if ("tzhour" %in% colnames(x_str))
@@ -63,7 +63,7 @@ format_field_matrix <- function(x,
   } else {
     x_str[,c("tzhour", "tzmin")] <- ""
   }
-  
+
   x_str[x_omit] <- ''
   apply(x_str, 1, paste0, collapse = '')
 }
@@ -71,11 +71,11 @@ format_field_matrix <- function(x,
 
 
 #' @importFrom pillar style_subtle style_na
-format_field <- function(x, digits = 2, leading_optional = FALSE, 
+format_field <- function(x, digits = 2, leading_optional = FALSE,
     fmt = if (leading_optional) "%.f" else sprintf("%%0%.f.f", digits)) {
 
   paste0(
-    if (leading_optional) 
+    if (leading_optional)
       pillar::style_subtle(strrep('0', digits - nchar(x %|NA|% 0L))),
     ifelse(is.na(x), pillar::style_na(sprintf(fmt, 0L)), sprintf(fmt, x)))
 }
--- R/utils.R
+++ R/utils.R
@@ -26,7 +26,7 @@ warn_partial <- function(x, ..., envir = parent.frame()) {
 
 
 spoken_list <- function(x, quote = FALSE, oxford = FALSE) {
-  if (isFALSE(quote)) quote <- ''
+  if (identical(quote, FALSE)) quote <- ''
   else if (isTRUE(quote)) quote <- '"'
 
   paste0(
@@ -40,15 +40,15 @@ spoken_list <- function(x, quote = FALSE, oxford = FALSE) {
 
 gmtoff <- function(tzone) {
   if (is.numeric(tzone)) return(tzone)
-  
+
   if (!tzone %in% OlsonNames())
     stop("Invalid timezone. Use `OlsonNames()` to see a list of valid timezones.")
-  
+
   date_stub <- lubridate::as_datetime("1970-01-01 00:00:00 GMT")
-  
+
   as.numeric(difftime(
-    date_stub, 
-    lubridate::force_tz(date_stub, tzone = tzone), 
+    date_stub,
+    lubridate::force_tz(date_stub, tzone = tzone),
     units = "secs")) / 60
 }
 
@@ -94,23 +94,23 @@ format_vector <- function(x) {
 #' @param drop whether to drop dimensions, a logical or numeric vector of
 #'   dimensions to drop
 #' @param envir environment in which to evaluate \code{[} call
-#' 
+#'
 extract <- function(x, ..., drop = TRUE, envir = parent.frame()) {
   args <- as.list(match.call())[-1]
   args <- args[names(args) == ""]
-  
+
   vals <- sapply(args, class) != "name"
   length_dims <- sapply(args, length) * (as.character(args) != "")
-  
+
   if (is.logical(drop))
     if (length(drop) == 1)
       return(do.call("[", append(append(list(x), args), list(drop = drop)), envir = envir))
     else if (length(drop) == length(dim(x)))
       drop <- which(drop)
-  
+
   if (!is.numeric(drop))
     stop('argument `drop` must be either logical of length 1 or numeric')
-  
+
   x <- do.call("[", append(append(list(x), args), list(drop = FALSE)), envir = envir)
   drop <- 1:length(dim(x)) %in% drop & dim(x) == 1
   dimnames_keep <- dimnames(x)[!drop]
@@ -126,7 +126,7 @@ extract <- function(x, ..., drop = TRUE, envir = parent.frame()) {
 #' @param x an object with dimnames<- defined
 #' @param dim the dimension to index
 #' @param ... names selected by which
-#' 
+#'
 .i <- function(x, dim, ...) {
   which(dimnames(x)[[dim]] %in% c(...))
 }
